8aa2d8439e3c8b1f2a4b7f034ab094ea63c456bc
yoyopch <117366105+yoyopch@users.noreply.github.com>
2023-05-24 20:57:58 +0200

    pull

--- SAE-VB/Jeu.vb
+++ SAE-VB/Jeu.vb
@@ -16,110 +16,112 @@ Public Class Jeu
         BtnBye.Visible = False
         lblPresent.ForeColor = Color.Blue
         lblPresentPlace.ForeColor = Color.Green
-        lblTemps.Text = tempsPourJouer
-        For i As Integer = 0 To caracteresJouable.Length - 1
-            LblCharJouable.Text &= caracteresJouable(i).c & " "
-        Next
-    End Sub
-    Private Sub txt_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt1.KeyPress, txt2.KeyPress, txt3.KeyPress, txt4.KeyPress, txt5.KeyPress
-        If e.KeyChar = vbBack Then
-            sender.BackColor = Color.White
-            Exit Sub
-        End If
-        If sender.Text.Length = 1 Then
-            e.Handled = True
-            Exit Sub
-        End If
-        For i As Integer = 0 To caracteresJouable.Length - 1
-            If (e.KeyChar = caracteresJouable(i).c) Then
-                sender.BackColor = Color.White
-                'SendKeys.Send("{TAB}") 'sympa mais fait comme si je cliquais sur tab, pas pratique pour @ par exemple
-                Exit Sub
-            End If
-        Next
-        e.Handled = True
-    End Sub
+    lblTemps.Text = tempsPourJouer
+    For i As Integer = 0 To caracteresJouable.Length - 1
+      LblCharJouable.Text &= caracteresJouable(i).c & " "
+    Next
+  End Sub
+  Private Sub txt_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txt1.KeyPress, txt2.KeyPress, txt3.KeyPress, txt4.KeyPress, txt5.KeyPress
+    If e.KeyChar = vbBack Then
+      sender.BackColor = Color.White
+      Exit Sub
+    End If
+    If sender.Text.Length = 1 Then
+      e.Handled = True
+      Exit Sub
+    End If
+    For i As Integer = 0 To caracteresJouable.Length - 1
+      If (e.KeyChar = caracteresJouable(i).c) Then
+        sender.BackColor = Color.White
+        'SendKeys.Send("{TAB}") 'sympa mais fait comme si je cliquais sur tab, pas pratique pour @ par exemple
+        Exit Sub
+      End If
+    Next
+    e.Handled = True
+  End Sub
 
-    Private Sub BtnBye_Click(sender As Object, e As EventArgs) Handles BtnBye.Click
-        FormAccueil.cboJoueur1.Text = "j2"
-        FormAccueil.cboJoueur2.Text = "j1"
-        FormAccueil.Show()
-        combin.Close()
-        Me.Close()
-    End Sub
+  Private Sub BtnBye_Click(sender As Object, e As EventArgs) Handles BtnBye.Click
+    FormAccueil.cboJoueur1.Text = "j2"
+    FormAccueil.cboJoueur2.Text = "j1"
+    FormAccueil.Show()
+    combin.Close()
+    Me.Close()
+  End Sub
 
-    Private Sub Form3_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
-        'peut être un peu brutale car j'empeche la fermeture du fichier
-        If Not correct Then
-            e.Cancel = True
-        End If
-        'FormAccueil.cboJoueur1.Text = "j2"
-        'FormAccueil.cboJoueur2.Text = "j1"
-    End Sub
+  Private Sub Form3_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+    'peut être un peu brutale car j'empeche la fermeture du fichier
+    If Not correct Then
+      e.Cancel = True
+    End If
+    'FormAccueil.cboJoueur1.Text = "j2"
+    'FormAccueil.cboJoueur2.Text = "j1"
+  End Sub
 
-    Private Sub Jeu_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
-        'peut être un peu brutale car j'empeche la fermeture du fichier
-        FormAccueil.cboJoueur1.Text = "j2"
-        FormAccueil.cboJoueur2.Text = "j1"
-        FormAccueil.Show()
-    End Sub
 
-    Private Sub btnEssaie_Click(sender As Object, e As EventArgs) Handles btnEssaie.Click
-        Dim valide = True
-        For i As Integer = 0 To PnlChar.Controls.Count - 1
-            If PnlChar.Controls(i).Text.Length <> 1 Then
-                PnlChar.Controls(i).BackColor = Color.Red
-                valide = False
-            End If
-        Next
-        If Not valide Then
-            Exit Sub
-        End If
-        If nbPropostions = nbTentatives Then
-            MsgBox("Dommage, vous n'avez pas trouvé ! ")
-            btnEssaie.Enabled = False
-            BtnBye.Visible = True
-            Exit Sub
-        End If
-        Dim correct = True
-        Dim caract As Caractere
-        tentatives(nbTentatives) = New Caractere(4) {}
-        For i As Integer = PnlChar.Controls.Count - 1 To 0 Step -1
-            caract = New Caractere
-            caract.c = PnlChar.Controls(i).Text(0)
-            If PnlChar.Controls(i).Text(0).Equals(combin.combineCache(i)) Then
-                PnlChar.Controls(i).BackColor = Color.Green
-                RTBTenta.SelectionColor = colorBon
-                caract.status = 2
-            ElseIf combin.combineCache.Contains(PnlChar.Controls(i).Text(0)) Then
-                PnlChar.Controls(i).BackColor = Color.Blue
-                caract.status = 1
-                RTBTenta.SelectionColor = colorIn
-                correct = False
 
-                'MsgBox("ok")
-            Else
-                caract.status = 0
-                'PnlChar.Controls(i).BackColor = colorMauvais
-                RTBTenta.SelectionColor = colorMauvais
-                correct = False
-            End If
+  Private Sub Jeu_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+    'peut être un peu brutale car j'empeche la fermeture du fichier
+    FormAccueil.cboJoueur1.Text = "j2"
+    FormAccueil.cboJoueur2.Text = "j1"
+    FormAccueil.Show()
+  End Sub
 
-            RTBTenta.AppendText(caract.c)
-            RTBTenta.AppendText("    ")
-            tentatives(nbTentatives)(i) = caract
-        Next
+  Private Sub btnEssaie_Click(sender As Object, e As EventArgs) Handles btnEssaie.Click
+    Dim valide = True
+    For i As Integer = 0 To PnlChar.Controls.Count - 1
+      If PnlChar.Controls(i).Text.Length <> 1 Then
+        PnlChar.Controls(i).BackColor = Color.Red
+        valide = False
+      End If
+    Next
+    If Not valide Then
+      Exit Sub
+    End If
+    If nbPropostions = nbTentatives Then
+      MsgBox("Dommage, vous n'avez pas trouvé ! ")
+      btnEssaie.Enabled = False
+      BtnBye.Visible = True
+      Exit Sub
+    End If
+    Dim correct = True
+    Dim caract As Caractere
+    tentatives(nbTentatives) = New Caractere(4) {}
+    For i As Integer = PnlChar.Controls.Count - 1 To 0 Step -1
+      caract = New Caractere
+      caract.c = PnlChar.Controls(i).Text(0)
+      If PnlChar.Controls(i).Text(0).Equals(combin.combineCache(i)) Then
+        PnlChar.Controls(i).BackColor = Color.Green
+        RTBTenta.SelectionColor = colorBon
+        caract.status = 2
+      ElseIf combin.combineCache.Contains(PnlChar.Controls(i).Text(0)) Then
+        PnlChar.Controls(i).BackColor = Color.Blue
+        caract.status = 1
+        RTBTenta.SelectionColor = colorIn
+        correct = False
 
-        RTBTenta.SelectAll()
-        RTBTenta.SelectionAlignment = HorizontalAlignment.Center
-        RTBTenta.AppendText(vbCrLf)
+        'MsgBox("ok")
+      Else
+        caract.status = 0
+        'PnlChar.Controls(i).BackColor = colorMauvais
+        RTBTenta.SelectionColor = colorMauvais
+        correct = False
+      End If
 
-        If correct Then
-            btnEssaie.Enabled = False
-            MsgBox("bravo")
-            BtnBye.Visible = True
-        End If
-        nbTentatives += 1
+      RTBTenta.AppendText(caract.c)
+      RTBTenta.AppendText("    ")
+      tentatives(nbTentatives)(i) = caract
+    Next
+
+    RTBTenta.SelectAll()
+    RTBTenta.SelectionAlignment = HorizontalAlignment.Center
+    RTBTenta.AppendText(vbCrLf)
+
+    If correct Then
+      btnEssaie.Enabled = False
+      MsgBox("bravo")
+      BtnBye.Visible = True
+    End If
+    nbTentatives += 1
         Me.Text = $"Il vous reste {nbPropostions - nbTentatives} coup(s)..."
         'For i As Integer = 0 To tentatives.Length - 1
         '    Dim s = ""
@@ -130,4 +132,7 @@ Public Class Jeu
         ReDim Preserve tentatives(nbTentatives)
     End Sub
 
+  Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+    lblTemps.Text = time
+  End Sub
 End Class
